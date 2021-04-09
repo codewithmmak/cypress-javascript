68002b5a14f9bdc906d7c624959d9a627ab210c6
qaloop <agiletester786@gmail.com>
2021-04-10 00:23:06 +0530

    Fixed Upload File script

--- test/cypress/integration/file_upload.spec.js
+++ test/cypress/integration/file_upload.spec.js
@@ -1,5 +1,3 @@
-const path = require("path");
-
 describe ('File Upload', () => {
 
     beforeEach (function(){
@@ -8,13 +6,14 @@ describe ('File Upload', () => {
 
     it ('File Upload Test', () => {
 
-        let fileName = "some-file.txt";
-        let filePath = path.join(process.cwd(), fileName);
-
+        let filePath = "test-data/Sample_Upload_File.txt";
+        
         cy.visit('upload')
         .get('h3').should('have.text', 'File Uploader')
-        .get('input[id="file-upload"]').should('be.visible')
-        .type('filePath')
+        .get('input[type="file"]').should('be.visible')
+        .attachFile(filePath)
+        .get('#file-submit').should('be.visible').click()
+        .get('#uploaded-files').contains('Sample_Upload_File.txt')
     })
 
 })
